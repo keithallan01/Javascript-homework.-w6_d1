6ddd9c55409c000d5239f89e930946b1d8ac2352
Keith Allan <keithallan01@gmail.com>
2019-01-08 09:55:05 +0000

    homework excersises updated

--- hw_solutions.js
+++ hw_solutions.js
@@ -12,7 +12,7 @@
 //
 // const verdict = declareMurderer();
 // console.log(verdict);
-
+//
 // The output will be 'Miss Scarlet', as this is a constant that cannot be changed in block scope.
 
 // #### Episode 2
@@ -30,7 +30,7 @@
 // changeMurderer();
 // const verdict = declareMurderer();
 // console.log(verdict);
-
+//
 // The output will be 'Mrs Peacock' as the muderer is overwritten- I got his wrong, assignment to contant variable error message popped up. It's a constant and cannot be overwritten.
 
 
@@ -50,13 +50,13 @@
 // const secondVerdict = `The murderer is ${murderer}.`;
 // console.log('Second Verdict: ', secondVerdict);
 // ```
-// The first verdict output will be Mrs Peacock as it is block scope.
-// The second verdict output will be Professor Plum as  it is Global scope
-
-
-// #### Episode 4
+// The first verdict output will be Mrs Peacock as it a local variable and is block scope.
+// The second verdict output will be Professor Plum as it is Global scope and is on the same level as line 40
 //
-// ```js
+//
+// // #### Episode 4
+// //
+// // ```js
 // let suspectOne = 'Miss Scarlet';
 // let suspectTwo = 'Professor Plum';
 // let suspectThree = 'Mrs. Peacock';
@@ -72,10 +72,10 @@
 // console.log(`Suspect three is ${suspectThree}.`);
 // this will return Mrs.Peacock as it's out of the block.
 // ```
-//
-// #### Episode 5
-//
-// ```js
+// //
+// // #### Episode 5
+// //
+// // ```js
 // const scenario = {
 //   murderer: 'Miss Scarlet',
 //   room: 'Kitchen',
@@ -95,10 +95,10 @@
 // console.log(verdict);
 // ```
 // The output will be Candle Stick as it's a constant that cannot be changed. I got this wrong. The Scenario is constant but the object can be overwritten- hence the output is revolver.
-
-// #### Episode 6
 //
-// ```js
+// // #### Episode 6
+// //
+// // ```js
 // let murderer = 'Colonel Mustard';
 //
 // const changeMurderer = function() {
@@ -119,8 +119,8 @@
 // const verdict = declareMurderer();
 // console.log(verdict);
 // ```
-// The murderer is Mrs. White. const changemurderer is contant, but the objectcan be overwritten, which is why Mrs. White is the murderer.
-
+// // The murderer is Mrs. White. const changemurderer is contant, but the objectcan be overwritten, which is why Mrs. White is the murderer.
+//
 // #### Episode 7
 //
 // ```js
@@ -152,10 +152,10 @@
 // ```
 // The murderer is Miss Scarlet. chngeMurderer and plotTwist can both be overwritten by this constant.
 // I was wrong, the output is Mr Green. This is because this is the first constant and cannot be overwritten.
-
-// #### Episode 8
 //
-// ```js
+// // #### Episode 8
+// //
+// // ```js
 // const scenario = {
 //   murderer: 'Mrs. Peacock',
 //   room: 'Conservatory',
@@ -191,24 +191,24 @@
 // const verdict = declareWeapon();
 // console.log(verdict);
 // ```
-// The weapon is the candle stick. scenario
-
+// The weapon is the candle stick. Mustard in the Dining room with a candle stick.
+//
 
 // #### Episode 9
 //
 // ```js
-let murderer = 'Professor Plum';
-
-if (murderer === 'Professor Plum') {
-  let murderer = 'Mrs. Peacock';
-}
-
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
-
-const verdict = declareMurderer();
-console.log(verdict);
-
-// ```
-// The murderer is Professor Plum. this is block scope. 
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // ```
+// The murderer is Professor Plum. this is block scope.
